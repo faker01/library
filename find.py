7912c3a9c90d66fc80e11d281bf9016d1e223b70
Maksimenko Aleksandr <S.maksimenko2004@mail.ru>
2021-02-01 11:30:33 +0200

    Update find.py

--- find.py
+++ find.py
@@ -1,1 +1,12 @@
 import os
+list_of_expansion = ['txt', 'doc', 'docx', 'pdf', 'epub', 'fb2']
+# .txt ; .doc ; .docx ; .pdf ; .epub ; .fb2
+
+def path_find_file(finded_files):
+    global list_of_expansion
+    result = []
+    for i in range(len(finded_files)):
+        expansion = finded_files[i].split('.')[1]
+        if expansion in list_of_expansion:
+            result.append(finded_files[i])
+    return result
